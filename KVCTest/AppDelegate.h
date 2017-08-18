eb9f1df4562332080e9ffb8260c356d21f25b494
CharlieSuP1 <supeng@p1.com>
2017-08-18 13:59:04 +0800

    Initial Commit

---
+++ KVCTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KVCTest
+//
+//  Created by supeng on 18/08/2017.
+//  Copyright © 2017 supeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KVCTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVCTest
+//
+//  Created by supeng on 18/08/2017.
+//  Copyright © 2017 supeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
